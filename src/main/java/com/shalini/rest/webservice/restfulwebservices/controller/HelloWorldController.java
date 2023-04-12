260b463f4976dff7ea5913d4d80df55d97fb1e5a
shaliniv10498 <shaliniv10498@gmail.com>
2023-04-12 09:27:22 +0530

    PathVariable Knowledge

--- src/main/java/com/shalini/rest/webservice/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/shalini/rest/webservice/restfulwebservices/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.shalini.rest.webservice.restfulwebservices.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shalini.rest.webservice.restfulwebservices.model.HelloWorldBean;
@@ -17,5 +18,9 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldbean() {
 		return new HelloWorldBean("This is a Hello World Bean");
 	}
-
+   
+	@GetMapping("/helloworld/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable("name")String name) {
+		return new HelloWorldBean(String.format("Hello World ,%s,",name));
+	}
 }
