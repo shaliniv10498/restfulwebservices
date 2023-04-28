32aac1bdae817ae57372d4da150aea3e03b98eee
shaliniv10498 <shaliniv10498@gmail.com>
2023-04-28 09:32:23 +0530

    HAL EXPLORER, DYNAMIC FILTERING, ACTUATORS

--- src/main/java/com/shalini/rest/webservice/restfulwebservices/controller/FilteringController.java
+++ src/main/java/com/shalini/rest/webservice/restfulwebservices/controller/FilteringController.java
@@ -3,9 +3,13 @@ package com.shalini.rest.webservice.restfulwebservices.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.shalini.rest.webservice.restfulwebservices.model.SomeBean;
 
 /**
@@ -28,5 +32,16 @@ public class FilteringController {
 			  new SomeBean("b1","b2","b3"),
 			  new SomeBean("c1","c2","c3")});
 	   }
+   
+   @GetMapping("filter/dynamic/field4")
+   public MappingJacksonValue filtering() {
+	   SomeBean obj = new SomeBean("v1","v2","v3","v4","v5");
+	   MappingJacksonValue mapping = new MappingJacksonValue(obj);
+	   SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field4");
+	   FilterProvider filters = new SimpleFilterProvider()
+				.addFilter("somebeanfilter", filter);
+	   mapping.setFilters(filters );
+	   return mapping;
+   }
    
 }
--- src/main/java/com/shalini/rest/webservice/restfulwebservices/model/SomeBean.java
+++ src/main/java/com/shalini/rest/webservice/restfulwebservices/model/SomeBean.java
@@ -1,22 +1,70 @@
 package com.shalini.rest.webservice.restfulwebservices.model;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties("field3") //class level annotation for
 //doing static filtering of fields [comma separated array of field name]
 //@JsonIgnoreProperties({"field3","field2"})
+@JsonFilter("somebeanfilter")
 public class SomeBean {
  private String field1;
  @JsonIgnore // static filtering field Level ANNOTATION FOR FILTERING OUT FIELDS
  private String field2;
  private String field3;
+ private String field4;
+ private String field5;
 public SomeBean(String field1, String field2, String field3) {
 	super();
 	this.field1 = field1;
 	this.field2 = field2;
 	this.field3 = field3;
 }
+
+
+public SomeBean(String field1, String field2, String field3, String field4, String field5) {
+	super();
+	this.field1 = field1;
+	this.field2 = field2;
+	this.field3 = field3;
+	this.field4 = field4;
+	this.field5 = field5;
+}
+
+
+/**
+ * @return the field4
+ */
+public String getField4() {
+	return field4;
+}
+
+
+/**
+ * @param field4 the field4 to set
+ */
+public void setField4(String field4) {
+	this.field4 = field4;
+}
+
+
+/**
+ * @return the field5
+ */
+public String getField5() {
+	return field5;
+}
+
+
+/**
+ * @param field5 the field5 to set
+ */
+public void setField5(String field5) {
+	this.field5 = field5;
+}
+
+
 /**
  * @return the field1
  */
