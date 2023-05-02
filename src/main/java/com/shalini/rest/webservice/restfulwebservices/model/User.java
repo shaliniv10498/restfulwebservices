074ffbe3bdfc00e1013cf824a112d40475b6ecc6
shaliniv10498 <shaliniv10498@gmail.com>
2023-05-02 08:53:33 +0530

    H2 DATABASE

--- src/main/java/com/shalini/rest/webservice/restfulwebservices/model/User.java
+++ src/main/java/com/shalini/rest/webservice/restfulwebservices/model/User.java
@@ -1,14 +1,22 @@
 package com.shalini.rest.webservice.restfulwebservices.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 
-
+@Entity
+@Table(name = "user_details")
 public class User {
     
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@NotNull
